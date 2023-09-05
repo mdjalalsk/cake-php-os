e7b9d4fde3190bf6bbf5538d086153af90950fb6
mdjalalsk <mdjalalsk02@gmail.com>
2023-09-05 19:59:22 +0600

    first

---
+++ hello.php
@@ -0,0 +1,3 @@
+<?php 
+echo"hello jddfgdgfdfhjsdkfhn fdsfkl fgf";
+echo"this  is tast in php fdgj i am finally setup";
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php 
+echo "hello Bangladenfgfgdfgdsghgfhhgfghfghgshgdhj dsfjsfuish tjeoritjreiotb iotureiot jalaal";
+echo "hello  ddsfsadfsahdfsfjsf ";
+echo "heloolo " 
+;
+echo "tmi amr jibon";
